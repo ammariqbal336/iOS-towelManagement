d96248dc34d065e40623031bf743248fe13a1b13
ammar <muhammad.ammar@appiskey.com>
2024-06-01 16:35:08 +0500

    Initial Commit

---
+++ TowelManagement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TowelManagement
+//
+//  Created by mac on 01/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TowelManagement/TowelManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TowelManagementApp.swift
+//  TowelManagement
+//
+//  Created by mac on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TowelManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
